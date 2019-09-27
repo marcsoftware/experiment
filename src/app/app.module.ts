711686b1bf8b1ba91d9ce782a617507b9dcafa85
Marc Melcher <marcdavidmelcher@gmail.com>
2019-09-27 13:10:06 -0500

    read text file with httpclient & printed to console

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { TypeComponent } from './type/type.component';
@@ -25,6 +26,7 @@ const appRoutes: Routes = [
     HomeComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     RouterModule.forRoot(
       appRoutes,
