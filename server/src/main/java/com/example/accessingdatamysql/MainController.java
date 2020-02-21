8503c18f00cf1d5d53557e739fe04a9495eee063
marcsoftware <you@example.com>
2020-02-21 11:49:08 -0700

    cors problem is solved - calls the spring api

--- server/src/main/java/com/example/accessingdatamysql/MainController.java
+++ server/src/main/java/com/example/accessingdatamysql/MainController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
+@CrossOrigin(origins = "*")
 @Controller	// This means that this class is a Controller
 @RequestMapping(path="/demo") // This means URL's start with /demo (after Application path)
 public class MainController {
@@ -29,7 +31,7 @@ public class MainController {
 		return "Saved";
 	}
 
-	@CrossOrigin(origins = "http://localhost:4201")
+	
 	@GetMapping(path="/all")
 	public @ResponseBody Iterable<User> getAllUsers() {
 		// This returns a JSON or XML with the users
