862c03c39e171083959d4d1575595dc228b20e53
marcsoftware <you@example.com>
2020-02-21 16:46:55 -0700

    fixed cors problem"

--- client/src/app/type/type.component.ts
+++ client/src/app/type/type.component.ts
@@ -31,7 +31,7 @@ dict=[];
   subject='1';
   ngOnInit() {
     this.subject = (this.route.snapshot.paramMap.get("subject"));
-    this.subject="assets/"+this.subject+".txt"
+    
 
     this.drawQuestion();
   }
@@ -53,10 +53,10 @@ dict=[];
   //=================================================================
   getAll() {
   
-   var works ='http://localhost:8080/demo/note/history';
-   var brocken='http://localhost:8080/demo/note/'+this.subject;
+  
+   var api='http://localhost:8080/demo/note/'+this.subject;
    
-    this.http.get(brocken, { responseType: 'text' })
+    this.http.get(api, { responseType: 'text' })
     .subscribe(data => {
       console.log(this);
       
@@ -69,7 +69,7 @@ dict=[];
   // this read file from assets folder
   //=================================================================
   readFile(){
-
+// TODO assets/+nameof file+.txt
     this.http.get(this.subject, { responseType: 'text' })
     .subscribe(data => {
       console.log(this);
